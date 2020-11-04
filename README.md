25d4c2ebd54cfe15763c41dcf256844472b5ccde
SweetDreamEng <sweetdream.eng0614@gmail.com>
2020-11-03 17:15:53 -0800

    update in readme

--- README.md
+++ README.md
@@ -10,30 +10,6 @@ ACS Backend API
 2. Setup .env file
 
     ``` sh
-    DB_CONNECTION=sqlsrv
-    DB_HOST=
-    DB_DATABASE=
-    DB_USERNAME=
-    DB_PASSWORD=
-
-    PASSPORT_GRANT_TYPE=password
-    PASSPORT_CLIENT_SECRET={client secret}
-    PASSPORT_CLIENT_ID={client id}
-
-3. Migrate database
-   ``` sh
-   php artisan migrate
-   ```
-
-4. Populate passport auth clients
-   ``` sh
-   php artisan passport:install
-   ```
-
-5. Seed ACS admin and roles
-   ``` sh
-   php artisan db:seed
-   ```
 
     ```
 6. Run the server
---
+++ app/Mail/WelcomeMail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class WelcomeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $address = 'ahmadyasser7outlook.com';
+        $subject = 'Welcome to ACS Digital Solutions!';
+        $name = 'Jane Doe';
+        
+        return $this->markdown('emails.customer-invitation')
+                    ->from($address, $name)
+                    ->subject($subject);
+    }
+}
