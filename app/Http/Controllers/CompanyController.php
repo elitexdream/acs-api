22f515d43d6812969d6d94749976774128840223
SweetDreamEng <sweetdream.eng0614@gmail.com>
2020-11-20 16:13:22 +0800

    sendgrid test

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -127,6 +127,23 @@ class CompanyController extends Controller
 
 	public function testMail()
 	{
-		Mail::to('scar20181228@gmail.com')->send(new CustomerInvitation());
+		$email = new \SendGrid\Mail\Mail();
+		$email->setFrom("al@machinecdn.com", "ACS");
+		$email->setSubject("Sending with Twilio SendGrid is Fun");
+		$email->addTo("scar20181228@gmail.com", "Example User");
+		$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+		$email->addContent(
+		    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+		);
+		$sendgrid = new \SendGrid(env('SENDGRID_API'));
+		try {
+		    $response = $sendgrid->send($email);
+		    print $response->statusCode() . "\n";
+		    print_r($response->headers());
+		    print $response->body() . "\n";
+		} catch (Exception $e) {
+		    echo 'Caught exception: '. $e->getMessage() ."\n";
+		}
+		// Mail::to('scar20181228@gmail.com')->send(new CustomerInvitation());
 	}
 }
---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mail;
+use App\Mail\CustomerInvitation;
+
+class MailController extends Controller
+{
+
+}
