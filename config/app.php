ebe4d009199f357c71af2dfd4d520a3dada90d62
SweetDreamEng <sweetdream.eng0614@gmail.com>
2020-11-21 09:58:25 +0800

    Invitation email on customer creation

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Laravel\Passport\PassportServiceProvider::class,
+        Sichikawa\LaravelSendgridDriver\SendgridTransportServiceProvider::class,
 
     ],
 
