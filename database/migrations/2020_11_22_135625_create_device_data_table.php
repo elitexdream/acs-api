3d935b2ca7437d9c346292ff4f01ad793ccfe025
SweetDreamEng <sweetdream.eng0614@gmail.com>
2020-11-26 22:23:50 +0800

    device_data migration updated

--- database/migrations/2020_11_22_135625_create_device_data_table.php
+++ database/migrations/2020_11_22_135625_create_device_data_table.php
@@ -16,11 +16,11 @@ class CreateDeviceDataTable extends Migration
         Schema::create('device_data', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('device_id');
-            $table->unsignedBigInteger('customer_id');
             $table->unsignedBigInteger('tag_id');
             $table->unsignedBigInteger('timestamp');
             $table->json('values');
-            $table->timestamps();
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('machine_id')->default(0);
         });
     }
 
