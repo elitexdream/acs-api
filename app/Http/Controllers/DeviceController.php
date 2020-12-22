fc86c7fe336d18d15ddba73b1bef7ca47fdefc44
SweetDreamEng <sweetdream.eng0614@gmail.com>
2020-12-22 20:37:47 +0800

    updated testAzureJson function to have acs-middleware endpoint from .env

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -486,7 +486,7 @@ class DeviceController extends Controller
         $client = new Client();
         try {
             $response = $client->post(
-                '127.0.0.1:3000/',
+                'http://127.128.0.1:3000/',
                 [
                     'json' => $request->all()
                 ]
