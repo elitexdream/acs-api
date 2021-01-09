973d1cb26f8e059a2735778221ed49ea72f4864b
SweetDreamEng <sweetdream.eng0614@gmail.com>
2021-01-09 10:27:11 +0800

    getProductWeight function updated

--- app/Http/Controllers/MachineController.php
+++ app/Http/Controllers/MachineController.php
@@ -215,19 +215,21 @@ class MachineController extends Controller
 		$targets = [];
 		$actuals = [];
 		
+
 		$target_object = $weight_objects->firstWhere('tag_id', 13);
 		$actual_object = $weight_objects->firstWhere('tag_id', 14);
 
+
 		if($target_object) {
 			$targets = json_decode($target_object->values);
-			foreach ($targets as $target) {
+			foreach ($targets as &$target) {
 				$target = $target / 1000;
 			}
 		}
 
 		if($actual_object) {
 			$actuals = json_decode($actual_object->values);
-			foreach ($actuals as $actual) {
+			foreach ($actuals as &$actual) {
 				$actual = $actual / 1000;
 			}
 		}
