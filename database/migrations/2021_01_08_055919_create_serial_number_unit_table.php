daddde7608309369b43a7a2edccb1c25ecab491e
SweetDreamEng <sweetdream.eng0614@gmail.com>
2021-01-25 11:45:00 +0800

    serial_number_unit migration updated

--- database/migrations/2021_01_08_055919_create_serial_number_unit_table.php
+++ database/migrations/2021_01_08_055919_create_serial_number_unit_table.php
@@ -21,6 +21,7 @@ class CreateSerialNumberUnitTable extends Migration
             $table->json('values');
             $table->unsignedBigInteger('customer_id')->default(0);
             $table->unsignedBigInteger('machine_id')->default(0);
+            $table->unsignedBigInteger('serial_number')->nullable();
         });
     }
 
