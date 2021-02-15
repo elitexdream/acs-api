dda7eff6d7a82ca0effc5705f7e179138c6741ae
SweetDreamEng <sweetdream.eng0614@gmail.com>
2021-02-16 06:06:42 +0800

    query cache removed

--- app/EnergyConsumption.php
+++ app/EnergyConsumption.php
@@ -7,10 +7,6 @@ use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class EnergyConsumption extends Model
 {
-    use QueryCacheable;
-
-    protected $cacheFor = 600;
-
     public $table = 'energy_consumptions';
     public $timestamps = false;
     public $fillable = ['id', 'device_id', 'tag_id', 'timestamp', 'values', 'customer_id', 'machine_id'];
--- app/Utilization.php
+++ app/Utilization.php
@@ -7,10 +7,6 @@ use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class Utilization extends Model
 {
-    use QueryCacheable;
-
-    protected $cacheFor = 600;
-    
     public $table = 'utilizations';
     public $timestamps = false;
     public $fillable = ['id', 'device_id', 'tag_id', 'timestamp', 'values', 'customer_id', 'machine_id'];
