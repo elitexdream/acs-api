b7f054d9618b9e800e4d5d9b68a36979ed6a0f7a
SweetDreamEng <sweetdream.eng0614@gmail.com>
2021-03-12 10:39:22 -0800

    fix value of divided_by

--- database/seeds/MachineTagsSeeder.php
+++ database/seeds/MachineTagsSeeder.php
@@ -14,15 +14,15 @@ class MachineTagsSeeder extends Seeder
     	DB::table('machine_tags')->delete();
 
         $machine_tags = [
-            [ 'name' => 'Blender Capability', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 17, 'type' => 'line', 'offset' => 0, 'divided_by' => 10 ],
-            [ 'name' => 'Process Rate', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 18, 'type' => 'line', 'offset' => 0, 'divided_by' => 10 ],
-            [ 'name' => 'Load cell A zero bits', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 20, 'type' => 'line', 'offset' => 0, 'divided_by' => 10 ],
-            [ 'name' => 'Load cell A cal bits', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 21, 'type' => 'line', 'offset' => 0, 'divided_by' => 10 ],
-            [ 'name' => 'Load cell B zero bits', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 22, 'type' => 'line', 'offset' => 0, 'divided_by' => 10 ],
-            [ 'name' => 'Load cell B cal bits', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 23, 'type' => 'line', 'offset' => 0, 'divided_by' => 10 ],
-            [ 'name' => 'Weigh hopper tare', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 24, 'type' => 'line', 'offset' => 0, 'divided_by' => 10 ],
+            [ 'name' => 'Blender Capability', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 17, 'type' => 'line', 'offset' => 0, 'divided_by' => 1 ],
+            [ 'name' => 'Process Rate', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 18, 'type' => 'line', 'offset' => 0, 'divided_by' => 1 ],
+            [ 'name' => 'Load cell A zero bits', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 20, 'type' => 'line', 'offset' => 0, 'divided_by' => 1 ],
+            [ 'name' => 'Load cell A cal bits', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 21, 'type' => 'line', 'offset' => 0, 'divided_by' => 1 ],
+            [ 'name' => 'Load cell B zero bits', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 22, 'type' => 'line', 'offset' => 0, 'divided_by' => 1 ],
+            [ 'name' => 'Load cell B cal bits', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 23, 'type' => 'line', 'offset' => 0, 'divided_by' => 1 ],
+            [ 'name' => 'Weigh hopper tare', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 24, 'type' => 'line', 'offset' => 0, 'divided_by' => 1 ],
             [ 'name' => 'Capacity Utilization', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 2, 'type' => 'line', 'offset' => 0, 'divided_by' => 10 ],
-            [ 'name' => 'Energy Consumption', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 3, 'type' => 'line', 'offset' => 0, 'divided_by' => 10 ],
+            [ 'name' => 'Energy Consumption', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 3, 'type' => 'line', 'offset' => 0, 'divided_by' => 1 ],
         ];
 
         DB::table('machine_tags')->insert($machine_tags);
