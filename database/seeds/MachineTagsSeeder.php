571f008ba79e464dd9bf7b6f963cc815ce10d3af
SweetDreamEng <sweetdream.eng0614@gmail.com>
2021-03-12 09:39:19 -0800

    add blender tags to seeder

--- database/seeds/MachineTagsSeeder.php
+++ database/seeds/MachineTagsSeeder.php
@@ -16,7 +16,13 @@ class MachineTagsSeeder extends Seeder
         $machine_tags = [
             [ 'name' => 'Blender Capability', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 17, 'type' => 'line' ],
             [ 'name' => 'Process Rate', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 18, 'type' => 'line' ],
-            [ 'name' => 'Load cell A zero bits', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 20, 'type' => 'line' ]
+            [ 'name' => 'Load cell A zero bits', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 20, 'type' => 'line' ],
+            [ 'name' => 'Load cell A cal bits', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 21, 'type' => 'line'],
+            [ 'name' => 'Load cell B zero bits', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 22, 'type' => 'line'],
+            [ 'name' => 'Load cell B cal bits', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 23, 'type' => 'line'],
+            [ 'name' => 'Weigh hopper tare', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 24, 'type' => 'line'],
+            [ 'name' => 'Capacity Utilization', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 2, 'type' => 'line'],
+            [ 'name' => 'Energy Consumption', 'configuration_id' => MACHINE_BD_BATCH_BLENDER, 'tag_id' => 3, 'type' => 'line'],
         ];
 
         DB::table('machine_tags')->insert($machine_tags);
