86ad2a28db0c272bd81043fd02b68462f708cf93
SweetDreamEng <sweetdream.eng0614@gmail.com>
2021-04-29 20:25:25 +0700

    update code to show hopper cleared time

---
+++ app/HopperClearedTime.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HopperClearedTime extends Model
+{
+    public $table = 'hopper_cleared_time';
+
+    protected $fillable = [
+        'serial_number', 'timestamp', 'last_cleared_time'
+    ];
+}
