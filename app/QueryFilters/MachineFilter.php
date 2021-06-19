1ec4f68e71f023cd6cc37106dfc36f74f809352f
SweetDreamEng <sweetdream.eng0614@gmail.com>
2021-06-19 22:22:45 +0530

    Device downtime api filtering added

---
+++ app/QueryFilters/MachineFilter.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\QueryFilters;
+
+
+use Closure;
+
+class MachineFilter
+{
+    public function handle($request, Closure $next)
+    {
+        if (!request()->has('machine_id') && !request()->has('serial_number')) {
+            return $next($request);
+        }
+        $builder = $next($request);
+        $builder->where(['devices.machine_id' => request('machine_id'),
+            'devices.serial_number' => request('serial_number')
+        ]);
+
+        return $builder;
+
+    }
+}
